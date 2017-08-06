90b691b537de6e481af56b28ec30d4514ef4db33
Naim Jeem <naim36208@gmail.com>
2017-08-06 17:12:45 +0600

    Basic weather app created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private http: Http) { }
+
+  cityName = '';
+  cityHumid = '';
+    searchCity() {
+      this.http.get('http://api.openweathermap.org/data/2.5/weather?APPID=0ba5e82df49a15a79cec569618c56215&q=' + this.cityName )
+      .subscribe (
+      (res: Response) => {
+      const weatherCity = res.json();
+      console.log(weatherCity);
+      this.cityHumid = weatherCity.main.humidity;
+    });
+  }
 }
